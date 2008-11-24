82022216b3f0f7d11a736ca22c9f3766d7a4db35
xiao.xf <xiao.xf@ed9c2fc4-ad36-11dd-8324-037e16aeffe8>
2008-11-24 01:57:20 +0000

    test add

--- trunk/test/fib.c
+++ trunk/test/fib.c
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
 int main(int argc,char** argv)
 {
+     if(argc<2)
+	  cout<<"input param!!"<<endl;
+     int count=atoi(argv[1]);
+     int fib1=1;
+     int fib2=1;
+     for(int i=0;i<count;i++)
+     {
+	  fib1=fib1+fib2;
+	  fib2=fib1+fib2;
+	  cout<<"fib1="<<fib1<<" fib2="<<fib2<<endl;
+	  double gold=((double)fib2)/((double)fib1);
+	  printf("gold=%.20f\n",gold);
+     }
+	  
 }
