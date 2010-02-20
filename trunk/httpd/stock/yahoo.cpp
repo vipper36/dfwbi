9d79ee4317e3d26f55dcd46a3511e5bd0cb7c4af
xiao.xf <xiao.xf@ed9c2fc4-ad36-11dd-8324-037e16aeffe8>
2010-02-20 10:05:36 +0000

    *** empty log message ***

--- trunk/httpd/stock/yahoo.cpp
+++ trunk/httpd/stock/yahoo.cpp
@@ -14,4 +14,40 @@ std::list<StockPrice> yahoo_stock::GetHisPrice(std::string stockName,boost::posi
 StockPrice yahoo_stock::GetCurPrice(std::string stockName)
 {
 }
-
+std::string yahoo_stock::CalUrl(std::string stockName,boost::posix_time::ptime &from, boost::posix_time::ptime& to,data_type type)
+{
+  std::string mcmd="http://203.209.255.240/test/kmin.py/wmData.znzDo?cmd=";
+  std::string dcmd="http://203.209.255.253/test/kline.py/data.znzDo?cmd=";
+  std::stringstream ss("");
+  date fday = from.date();
+  date tday = to.date();
+  switch(type)
+    {
+    case DAY:
+      ss<<dcmd;
+      ss<<TransName(stockName);
+      ss<<"|";
+      for(greg_year ty=fday.year();ty<=tday.year();ty++)
+	{
+	  ss<<ty<<",";
+	}
+      ss<<"|";
+      break;
+    case M1:
+    case M5:
+    case M15:
+    case M30:
+    case M60:
+      ss<<dcmd;
+      ss<<type<<"|";
+      ss<<TransName(stockName);
+      break;
+    default:
+      break;
+    }
+  return ss.str();
+}
+std::string TransName(std::string stockName)
+{
+ 
+}
