9b44af7394f9d73cbe72528348c861e354f0d102
xiao.xf <xiao.xf@ed9c2fc4-ad36-11dd-8324-037e16aeffe8>
2010-02-20 10:05:47 +0000

    *** empty log message ***

--- httpd/stock/yahoo.h
+++ httpd/stock/yahoo.h
@@ -1,6 +1,8 @@
 #ifndef _STATUS_H_
 #define _STATUS_H_
 #include "stock_inter.hpp"
+#include "HttpClient.h"
+#include "HttpResponse.h"
 #ifdef __cplusplus 
 extern "C"
 {
@@ -17,8 +19,12 @@ public:
 	  }
      ~yahoo_stock()
 	  {}
-     virtual std::list<StockPrice> GetHisPrice(std::string stockName,boost::posix_time::ptime &from, boost::posix_time::ptime& to,data_type type);
+     virtual std::list<StockPrice> &GetHisPrice(std::string stockName,boost::posix_time::ptime &from, boost::posix_time::ptime& to,data_type type);
      virtual StockPrice GetCurPrice(std::string stockName);
 private:
+     std::string CalUrl(std::string stockName,boost::posix_time::ptime &from, boost::posix_time::ptime& to,data_type type);
+     std::string TransName(std::string stockName);
+     std::list<StockPrice> _priceList;
+     HttpClient _client;
 };
 #endif
